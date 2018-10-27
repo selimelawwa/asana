8ab2560bf8c1f7ccd992558e38d8fa937254ee8e
selimelawwa <selimalawwa335@gmail.com>
2018-10-27 19:29:18 +0200

    try to fix issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def will_paginate(collection_or_options = nil, options = {})
-    if collection_or_options.is_a? Hash
-      options, collection_or_options = collection_or_options, nil
-    end
-    unless options[:renderer]
-      options = options.merge :renderer => Pagination
-    end
-    super *[collection_or_options, options].compact
-  end
+  # def will_paginate(collection_or_options = nil, options = {})
+  #   if collection_or_options.is_a? Hash
+  #     options, collection_or_options = collection_or_options, nil
+  #   end
+  #   unless options[:renderer]
+  #     options = options.merge :renderer => Pagination
+  #   end
+  #   super *[collection_or_options, options].compact
+  # end
 end
