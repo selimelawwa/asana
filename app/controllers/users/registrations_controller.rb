f332272ab6093d6e09ae66ddf8b2328639d5454e
selimelawwa <selimalawwa335@gmail.com>
2018-10-27 15:36:06 +0200

    add jquery to allow only submission with not blan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,9 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :clear_password_submission, only: [:update]
+  prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy, :edit_password]
 
   # GET /resource/sign_up
   # def new
@@ -20,9 +22,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+
+    if resource_updated
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  def edit_password
+  end
+
+  def update_password
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    if user_params[:password].blank? || user_params[:password_confirmation].blank?
+      render "edit_password"
+    elsif resource.update_with_password(user_params)
+      bypass_sign_in resource, scope: resource_name
+      redirect_to root_path
+    else
+      render "edit_password"
+    end
+  end
+  
+  
 
   # DELETE /resource
   # def destroy
@@ -38,7 +69,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def clear_password_submission
+    params[:user].delete(:password)
+    params[:user].delete(:password_confirmation)
+    params[:user].delete(:current_password)
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,9 +87,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys:  [:city, :birthday, :name])
+  end
+
+  def user_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users,skip: [:passwords], controllers: {
         sessions: 'users/sessions',
         registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'users/edit/password', to: 'users/registrations#edit_password', as: 'edit_user_password'
+    put 'users/update_password', to: 'users/registrations#update_password', as: 'update_user_password'
+  end
   get 'welcome/index'
   root 'welcome#index'
 end
