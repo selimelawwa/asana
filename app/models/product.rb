b3af0e684e7555c28f2b5ce60adfa447e6d59017
selimelawwa <selimalawwa335@gmail.com>
2018-10-28 22:49:10 +0200

    add option to fill image with white background on resize & maintain aspect ratio

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-  has_attached_file :main_photo, styles: { medium: "300x300>", thumb: "100x100>" }
-
+  has_attached_file :main_photo, styles: { medium: "300x300>", thumb: "100x100>" },
+    :convert_options => {:medium => "-gravity center -extent 300x300"}
+    
   validates_attachment_content_type :main_photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 end
