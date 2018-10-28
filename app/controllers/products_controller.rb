29683bc1c894ddd3a47d6b93e761b7245b228b67
selimelawwa <selimalawwa335@gmail.com>
2018-10-29 00:01:37 +0200

    add edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,29 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:destroy, :destroy]
+  before_action :set_product, only: [:destroy, :edit, :update]
   
   def index
     @products = Product.all.paginate(:page => params[:page], :per_page => 4)
     @custom_paginate_renderer = custom_paginate_renderer
   end
+
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(product_params)
     if @product.save
