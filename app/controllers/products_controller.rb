4f8fa210882f448f628db40f15c326c1ee92378b
selimelawwa <selimalawwa335@gmail.com>
2018-11-30 22:00:55 +0200

    wip fix form and product search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,7 +52,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :gender, :main_photo, :description, :price, :category_ids)
+    params.require(:product).permit(:name, :gender, :main_photo, :description, :price, :category_ids, :sub_category_ids)
   end
 
   def set_product
