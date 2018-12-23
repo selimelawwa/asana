3bfb110853f87809e2615ddb7398173dbae19425
Mahmoud Abdel Razik <mahmoudabdelrazik95@gmail.com>
2018-12-23 21:22:36 +0200

    Added images

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_attached_file :main_photo, styles: { medium: "300x300>", thumb: "100x100>" },
     :convert_options => {:medium => "-gravity center -extent 300x300"}
     
-  validates_attachment_content_type :main_photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates_attachment_content_type :main_photo, :content_type => ["image/jpg", "image/jpeg", "image/png",]
 
   private
   def ransackable_attributes
