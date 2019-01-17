65a7b3574481cf67a7c084c49a830cd90f60a717
selimelawwa <selimalawwa335@gmail.com>
2019-01-18 01:55:11 +0200

    using cookies

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,26 +1,26 @@
 module GuestsHelper
   def current_or_guest_user
     if current_user
-      if session[:guest_user_id] && session[:guest_user_id] != current_user.id
+      if cookies.signed[:guest_user_id] && cookies.signed[:guest_user_id] != current_user.id
         logging_in
         # reload guest_user to prevent caching problems before destruction
         guest_user(with_retry = false).try(:reload).try(:destroy)
-        session[:guest_user_id] = nil
+        cookies.delete :guest_user_id
       end
       current_user
     else
       guest_user
     end
   end
-  # find guest_user object associated with the current session,
+  # find guest_user object associated with the saved cookie,
   # creating one as needed
   def guest_user(with_retry = true)
     # Cache the value the first time it's gotten.
-    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+    @cached_guest_user ||= User.find(cookies.signed[:guest_user_id] ||= create_guest_user.id)
 
-  rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
-     session[:guest_user_id] = nil
-     guest_user if with_retry
+  rescue ActiveRecord::RecordNotFound # if cookies.signed[:guest_user_id] invalid
+    cookies.delete :guest_user_id
+    guest_user if with_retry
   end
 
   private
@@ -46,7 +46,7 @@ module GuestsHelper
   def create_guest_user
     u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com", guest: true)
     u.save!(:validate => false)
-    session[:guest_user_id] = u.id
+    cookies.signed.permanent[:guest_user_id] = u.id
     u
   end
 end
