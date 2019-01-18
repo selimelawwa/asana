e258b02b4bb9c24a06c10bfb356a6d07eb431588
selimelawwa <selimalawwa335@gmail.com>
2019-01-19 00:54:41 +0200

    try different way

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: ‘_some_session’, expires: 2.year.from_now
+Rails.application.config.session_store :cookie_store, key: '_some_session', expires: 2.year.from_now
