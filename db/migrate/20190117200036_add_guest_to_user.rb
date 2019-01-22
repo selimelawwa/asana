d3dc30ab188a5866db8061a59029364592e16e6b
selimelawwa <selimalawwa335@gmail.com>
2019-01-22 02:36:02 +0200

    handle issue in migration

--- db/migrate/20190117200036_add_guest_to_user.rb
+++ db/migrate/20190117200036_add_guest_to_user.rb
@@ -1,5 +1,5 @@
 class AddGuestToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :guest, :boolean, default: false
+    add_column :users, :guest, :boolean, default: false unless column_exists?(:users, :guest)
   end
 end
