4a75cb52b5e76f05eaf3cf1d698656c3072b2324
selimelawwa <selimalawwa335@gmail.com>
2019-02-01 16:30:07 +0200

    fix issue in related_products , jumbo tron category not required

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
     @slides = Slide.first(5)
     @jumbotron = Jumbotron.first
     if @jumbotron.present?
-      @category1_name = Category.find(@jumbotron.category_id_1)&.name
-      @category2_name = Category.find(@jumbotron.category_id_2)&.name
+      @category1_name = Category.find(@jumbotron.category_id_1)&.name if @jumbotron.category_id_1.present?
+      @category2_name = Category.find(@jumbotron.category_id_2)&.name if @jumbotron.category_id_2.present?
     end
     @new_arrivals = Product.published.order(created_at: :desc).first(10).shuffle.first(4)
   end
--- app/models/jumbotron.rb
+++ app/models/jumbotron.rb
@@ -1,7 +1,7 @@
 class Jumbotron < ApplicationRecord
   has_attached_file :photo
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-  validates :photo, :category_id_1, :category_id_2, presence: true
+  validates :photo, presence: true
   validate :validate_categories_not_the_same
 
   def validate_categories_not_the_same
